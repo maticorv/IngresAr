5cc7c8e849b8b00caa75d86ea6b627dce136d67d
Matias Corvalan <matias_corvalan@outlook.com>
2019-08-12 16:12:41 -0300

    signin page added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', loadChildren: './security/login/login.module#LoginPageModule' },
   { path: 'forgot', loadChildren: './security/forgot/forgot.module#ForgotPageModule' },
   { path: 'singin', loadChildren: './security/singin/singin.module#SinginPageModule' },
