f9731494701cd3967434ea01f6c0a089f3a59db2
Gaston <gaston.silveyra87@gmail.com>
2019-08-22 17:16:31 -0300

    Transport changed

--- src/app/ingreso/transport/transport.page.ts
+++ src/app/ingreso/transport/transport.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-transport',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TransportPage implements OnInit {
 
+  index;
+
+  vehiculos = [
+    {
+      marca: 'Ford',
+      modelo: 'Focus',
+      patente: '01x01',
+    },
+    {
+      marca: 'Chevrolet',
+      modelo: 'Astra',
+      patente: '01x02',
+    },
+    {
+      marca: 'Peugeot',
+      modelo: '207',
+      patente: '01x03',
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  siguiente(form: NgForm) {
+    console.log(this.index);
+
   }
 
 }
