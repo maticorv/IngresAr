8c69c9e37ae1a671baac6721ba9a69c260c843f7
Gaston <gaston.silveyra87@gmail.com>
2019-08-22 17:18:12 -0300

    Type of visit changed

--- src/app/ingreso/typeofingress/typeofingress.page.ts
+++ src/app/ingreso/typeofingress/typeofingress.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-typeofingress',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TypeofingressPage implements OnInit {
 
-  constructor() { }
+  constructor( private menuCtrl: MenuController) { 
+    this.menuCtrl.enable(false);
+  }
 
   ngOnInit() {
   }
