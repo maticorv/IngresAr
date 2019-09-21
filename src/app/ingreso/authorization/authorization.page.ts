c97405386b215b78a21f2abc4a9f9d7120fcc8d3
Gaston <gaston.silveyra87@gmail.com>
2019-09-21 19:06:38 -0300

    authorization changes

--- src/app/ingreso/authorization/authorization.page.ts
+++ src/app/ingreso/authorization/authorization.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ServiceService } from '../../services/service.service';
+import { Router } from '@angular/router';
+import { Autorizador } from 'src/app/classes/autorizador';
+import { ToastController } from '@ionic/angular';
+import { Acompañante } from 'src/app/classes/acompañante';
 
 @Component({
   selector: 'app-authorization',
@@ -7,10 +12,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthorizationPage implements OnInit {
 
-  constructor() { }
+  // tslint:disable-next-line: max-line-length
+  constructor(private service: ServiceService, private router: Router, private autorizador: Autorizador, private toastController: ToastController, private acompañante: Acompañante) { }
 
-  i: string;
+  nombreAutorizador: string;
   dni: string;
+  cantidad = 0;
 
   persona = [
     {
@@ -28,8 +35,35 @@ export class AuthorizationPage implements OnInit {
   }
 
   imprimir() {
-    console.log(this.i);
-    console.log(this.dni);
+    // console.log(this.i);
+    // console.log(this.dni);
+    console.log(this.cantidad);
+  }
+
+  procesarIngreso() {
+    this.autorizador.nombreAutorizador = this.nombreAutorizador;
+    this.acompañante.cantidadAcompañante = this.cantidad;
+    this.service.postPlanillaIngreso().subscribe(data => {
+      console.log(data);
+      this.presentToast('El ingreso se ha realizado correctamente');
+    },
+    (error => {console.log(error);
+               this.presentToast('El ingreso no se ha podido realizar, intente nuevamente');
+  }));
+  }
+
+  async presentToast(me: string) {
+    const toast = await this.toastController.create({
+      position: 'middle',
+      color: 'dark',
+      duration: 2000,
+      message: me,
+    });
+    toast.present();
+    setTimeout(() => {
+      this.router.navigateByUrl('/startmenu');
+      },
+      2000);
   }
 
 }
