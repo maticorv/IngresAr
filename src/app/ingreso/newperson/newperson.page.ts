e5260182f7e25456feebef7277c232e501b1152c
Gaston <gaston.silveyra87@gmail.com>
2019-09-21 19:08:47 -0300

    newperson changes

--- src/app/ingreso/newperson/newperson.page.ts
+++ src/app/ingreso/newperson/newperson.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Personas } from 'src/app/classes/persona';
+import { ServiceService } from '../../services/service.service';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-newperson',
@@ -7,9 +11,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewpersonPage implements OnInit {
 
-  constructor() { }
+  nombrePersona: string;
+  apellidoPersona: string;
+  dniPersona: number;
+  telefonoPersona: number;
+
+  // tslint:disable-next-line: max-line-length
+  constructor(private router: Router, private persona: Personas, private service: ServiceService, private toastController: ToastController) { }
 
   ngOnInit() {
+  }
+  crearPersona() {
+    this.persona.nombrePersona = this.nombrePersona;
+    this.persona.apellidoPersona = this.apellidoPersona;
+    this.persona.dniPersona = this.dniPersona;
+    this.persona.telefonoPersona = this.telefonoPersona;
+    this.service.postPersona(this.nombrePersona, this.apellidoPersona, this.dniPersona, this.telefonoPersona).subscribe(data => {
+      console.log(data);
+      this.presentToast('El usuario se ha creado correctamente');
+      // this.router.navigateByUrl('/destination');
+    },
+    (error) => { console.log(error);
+                 this.presentToast('El usuario no se ha podido crear, intente nuevamente');
+    });
+  }
+
+  async presentToast(me: string) {
+    const toast = await this.toastController.create({
+      position: 'middle',
+      color: 'dark',
+      duration: 2000,
+      message: me,
+    });
+    toast.present();
+    setTimeout(() => {
+      this.router.navigateByUrl('/destination');
+      },
+      2000);
   }
 
 }
