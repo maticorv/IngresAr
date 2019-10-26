593f6c4311614472cd93f00c3c1d9e5ecf0a39e3
Gaston <gaston.silveyra87@gmail.com>
2019-10-26 17:42:20 -0300

    add friend page agregado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -81,6 +81,7 @@ const routes: Routes = [
   { path: 'detalle-evento/:id', loadChildren: './funcionesMenu/eventos/detalle-evento/detalle-evento.module#DetalleEventoPageModule' },
   { path: 'generate-qr', loadChildren: './Owner/authorizations/generate-qr/generate-qr.module#GenerateQrPageModule' },
   { path: 'view-friend-list/:id', loadChildren: './Owner/events/view-friend-list/view-friend-list.module#ViewFriendListPageModule' },  { path: 'add-friend', loadChildren: './Owner/events/ViewFriendList/add-friend/add-friend.module#AddFriendPageModule' },
+  { path: 'add-friend', loadChildren: './Owner/events/view-friend-list/add-friend/add-friend.module#AddFriendPageModule' },
 
 
 
