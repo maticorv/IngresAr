46c282580d3da17419b5d248fded2906b9d85abf
Gaston <gaston.silveyra87@gmail.com>
2019-10-31 22:20:11 -0300

    aliminar guardia agregado

--- src/app/Administrador/manage-guard/manage-guard.page.ts
+++ src/app/Administrador/manage-guard/manage-guard.page.ts
@@ -11,6 +11,9 @@ import { Router } from '@angular/router';
 })
 export class ManageGuardPage implements OnInit {
   guardias: Persona[];
+  authorities = [
+    'ROLE_USER'
+];
 
   constructor(private service: ServiceService, private router: Router) { }
 
@@ -23,11 +26,25 @@ export class ManageGuardPage implements OnInit {
   }
 
   getGuardias() {
-    this.service.getPersonaRol('ROLE_GUARDIA').subscribe(data => {this.guardias = data; } );
+    this.service.getPersonaRol('ROLE_GUARDIA').subscribe(data => {
+      this.guardias = data;
+      console.log(this.guardias);
+    },
+    (error) => {console.log(error);
+    });
   }
 
   crearGuardia() {
     this.router.navigateByUrl('/search-guard');
+  }
+
+  borrar(i) {
+    // tslint:disable-next-line: max-line-length
+    this.service.cambiarRol(this.guardias[i].personaUser.id, this.guardias[i].personaUser.login, this.guardias[i].personaUser.firstName, this.guardias[i].personaUser.lastName, this.guardias[i].personaUser.email, this.guardias[i].personaUser.imageUrl, this.guardias[i].personaUser.activated, this.guardias[i].personaUser.langKey, this.guardias[i].personaUser.createdBy, this.guardias[i].personaUser.createdDate, this.guardias[i].personaUser.lastModifiedBy, this.guardias[i].personaUser.lastModifiedDate, this.authorities).subscribe(data => {
+      console.log(data);
+      this.getGuardias();
+    },
+    (error) => {console.log(error);})
   }
 
 }
