446810d93c628600bcfa4c3dbcd1d7d5b9875a42
Matias Corvalan <matias_corvalan@outlook.com>
2019-12-01 17:27:39 -0300

    servicio de one signal agregado

--- src/app/services/push.service.ts
+++ src/app/services/push.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { OneSignal } from '@ionic-native/onesignal/ngx';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PushService {
 
-  constructor() { }
+  constructor(private oneSignal: OneSignal) {
+  }
+  configuracionInicial() {
+    // id de one signal y id de firebase
+    this.oneSignal.startInit('89349907-3fb7-4dd0-aec7-73b78fafb0e1', '376965492763');
+
+    this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.InAppAlert);
+
+    this.oneSignal.handleNotificationReceived().subscribe((data) => {
+      console.log('data :', data);
+      // do something when notification is received
+    });
+
+    this.oneSignal.handleNotificationOpened().subscribe((data) => {
+      // do something when a notification is opened
+      console.log('data :', data);
+      });
+
+    this.oneSignal.endInit();
+
+  }
 }
